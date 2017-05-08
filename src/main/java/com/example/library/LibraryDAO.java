a8e92a73abb658a817c103fef5fd82d3bf812366
TV <ab@cde.com>
2017-05-09 01:50:40 +0300

    sorting by title

--- src/main/java/com/example/library/LibraryDAO.java
+++ src/main/java/com/example/library/LibraryDAO.java
@@ -1,6 +1,8 @@
 package com.example.library;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +29,19 @@ public class LibraryDAO {
 		if (title != null && title.length() > 0) {
 			queryKey += " intitle:" + title;
 		}
-		if(queryKey.length() == 0) {
+		if (queryKey.length() == 0) {
 			return new ArrayList<LibraryItem>();
 		}
-		return getBooksByKey(queryKey);
+		return sortBooksByTitle(getBooksByKey(queryKey));
 	}
 
+	private List<LibraryItem> sortBooksByTitle(List<LibraryItem> books) {
+		Collections.sort(books, new Comparator<LibraryItem>() {
+			@Override
+			public int compare(LibraryItem o1, LibraryItem o2) {
+				return o1.getVolumeInfo().getTitle().compareTo(o2.getVolumeInfo().getTitle());
+			}
+		});
+		return books;
+	}
 }
